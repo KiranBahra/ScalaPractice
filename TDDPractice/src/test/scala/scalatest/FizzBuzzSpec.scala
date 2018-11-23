86a87d341081d829994fe67d3bcff9a531ab518a
Bahra <kiran.bahra@capgemini.com>
2018-11-23 22:49:39 +0000

    pushing tdd code up to repo

--- TDDPractice/src/test/scala/scalatest/FizzBuzzSpec.scala
+++ TDDPractice/src/test/scala/scalatest/FizzBuzzSpec.scala
@@ -13,10 +13,10 @@ class TwoNumbersSpec extends FlatSpec{
 
 
 }
- */
+
 class FizzBuzzSpec extends FlatSpec {
   "fizzBuzz" should "divide by 3 exactly and return the word fizz" in {
     val test1 = new FizzBuzz()
     assert(test1.fizzBuzz === "fizz")
   }
-}
+}*/
